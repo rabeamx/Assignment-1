e288d1617cce528b10a0872df9c8e9ceb816eef9
nazmunx <rabeakter02@gmail.com>
2022-03-20 15:08:55 +0600

    upload area file

---
+++ area.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *  Area Formulas - 02
+ */
+
+ function getArea($type = 'r', $length, $width = null)
+ {
+     $area = '';
+     $area_type = '';
+
+     switch ($type) {
+         case 'r':
+            $area_type = "rectangular";
+            $area = $length * $width;
+            break;
+
+         case 's':
+            $area_type = "square";
+            $area = $length * $length;
+            break;
+
+         case 'c':
+            $area_type = "circle";
+            $area = 3.1416 * ($length * $length);
+            break;
+
+         default:
+           $area_type = "undefined";
+           $area = "undefined";
+           break;
+     }
+
+     return "<h5 style = 'text-align:center; color:#6495ED; font-family:montserrat;'>Area of this {$area_type} : {$area}</h5>";
+ }
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
 
 /**
- * Result System
+ * Result System - 01
  */
 include_once "./result.php";
 
-result(44);
+result(100);
 
+/**
+ *  Area Formulas - 02
+ */
+include_once "./area.php";
 
+echo getArea('r', 5, 9);
 
 ?>
